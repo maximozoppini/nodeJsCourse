e8e741c1784a4dfd14bf7745621909c186ff12f2
Maximo Zoppini <maximozoppini@gmail.com>
2022-09-14 00:11:51 -0300

    entregable 4, correcciones para escuchar error de puerto

--- Entregables/Entregable_4/index.js
+++ Entregables/Entregable_4/index.js
@@ -9,10 +9,13 @@ const PORT = 8080;
 const listener = app.listen(PORT, function () {
     console.log(`Your app is listening to port: ${listener.address().port}`);
 });
+listener.on("error", (error) => console.log(`error en el servidor: ${error}`));
 
+//recurso estatico
 app.use("/public", express.static("public"));
-
+//root de la app
 app.get("/", (req, res) => {
     res.send("hola, server ONLINE");
 });
+//router para los productos
 app.use("/api/productos", routerProducto);
